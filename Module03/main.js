99b886b3a6beb1815763816d0c0fdeaeb7df7855
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-22 13:18:22 +0000

    file setup

---
+++ Module03/main.js
@@ -0,0 +1,9 @@
+const paragraph = document.createElement('p');
+paragraph.innerText = "I'm a paragraph";
+paragraph.style.fontSize = "18px";
+paragraph.style.fontFamily = 'sans-serif';
+paragraph.style.border = '1px solid orange';
+paragraph.style.padding = '30px'
+
+
+document.body.appendChild(paragraph);
